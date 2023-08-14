0ef309eb6f98b2c570a73e9a35e4fcd1eb4b2032
OlafPetersen <federicop526@gmail.com>
2023-08-14 20:27:47 -0300

    Pre3

--- main.js
+++ main.js
@@ -1,48 +1,54 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const personas = [
-      { nombre: "Juan", edad: 25 },
-      { nombre: "Maria", edad: 32 },
-      { nombre: "Pedro", edad: 18 }
-    ];
+  let personas = JSON.parse(localStorage.getItem("personas")) || [
+    { nombre: "Juan", edad: 25 },
+    { nombre: "Maria", edad: 32 },
+    { nombre: "Pedro", edad: 18 }
+  ];
 
-    function mostrarTabla() {
-      console.table(personas);
+  function guardarPersonas() {
+    localStorage.setItem("personas", JSON.stringify(personas));
+  }
+
+  function mostrarTabla() {
+    console.table(personas);
+  }
+
+  function agregarPersona(nombre, edad) {
+    personas.push({ nombre, edad });
+    guardarPersonas();
+  }
+
+  function buscarPersona(nombre) {
+    return personas.filter(persona => persona.nombre === nombre);
+  }
+
+  function ordenarPorEdad() {
+    personas.sort((a, b) => a.edad - b.edad);
+    guardarPersonas();
+  }
+
+  const boton = document.querySelector("#boton");
+  boton.addEventListener("click", () => {
+    const accion = prompt("¿Qué acción deseas realizar? (mostrar, agregar, buscar, ordenar)");
+    switch (accion) {
+      case "mostrar":
+        mostrarTabla();
+        break;
+      case "agregar":
+        const nombre = prompt("Ingresa el nombre de la persona");
+        const edad = parseInt(prompt("Ingresa la edad de la persona"));
+        agregarPersona(nombre, edad);
+        break;
+      case "buscar":
+        const nombreBusqueda = prompt("Ingresa el nombre de la persona a buscar");
+        const resultado = buscarPersona(nombreBusqueda);
+        console.table(resultado);
+        break;
+      case "ordenar":
+        ordenarPorEdad();
+        break;
+      default:
+        alert("Acción no válida");
     }
-
-    function agregarPersona(nombre, edad) {
-      personas.push({ nombre, edad });
-    }
-
-    function buscarPersona(nombre) {
-      return personas.filter(persona => persona.nombre === nombre);
-    }
-
-    function ordenarPorEdad() {
-      personas.sort((a, b) => a.edad - b.edad);
-    }
-
-    const boton = document.querySelector("#boton");
-    boton.addEventListener("click", () => {
-      const accion = prompt("¿Qué acción deseas realizar? (mostrar, agregar, buscar, ordenar)");
-      switch (accion) {
-        case "mostrar":
-          mostrarTabla();
-          break;
-        case "agregar":
-          const nombre = prompt("Ingresa el nombre de la persona");
-          const edad = parseInt(prompt("Ingresa la edad de la persona"));
-          agregarPersona(nombre, edad);
-          break;
-        case "buscar":
-          const nombreBusqueda = prompt("Ingresa el nombre de la persona a buscar");
-          const resultado = buscarPersona(nombreBusqueda);
-          console.table(resultado);
-          break;
-        case "ordenar":
-          ordenarPorEdad();
-          break;
-        default:
-          alert("Acción no válida");
-      }
-    });
   });
+});
